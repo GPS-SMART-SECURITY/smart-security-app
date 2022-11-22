d73b65460c197c5541f91626958e5e2a9ebd819d
Daniel Fernandes <danielalmfernandes@hotmail.com>
2022-11-22 18:55:36 +0000

    database configuration and models

---
+++ backend/SmartSecurity/src/main/java/com/isec/gps41/SmartSecurity/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.isec.gps41.SmartSecurity.repository;
+
+import com.isec.gps41.SmartSecurity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
