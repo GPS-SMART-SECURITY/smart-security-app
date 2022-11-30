87f262b5b6fb0a731c35a22119e1646aedc1c70a
jorhug <102927982+jorhug@users.noreply.github.com>
2022-11-30 17:35:24 +0000

    Models Service

--- backend/SmartSecurity/src/main/java/com/isec/gps41/SmartSecurity/repository/DivisionRepository.java
+++ backend/SmartSecurity/src/main/java/com/isec/gps41/SmartSecurity/repository/DivisionRepository.java
@@ -2,6 +2,19 @@ package com.isec.gps41.SmartSecurity.repository;
 
 import com.isec.gps41.SmartSecurity.model.Division;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface DivisionRepository extends JpaRepository<Division, Long> {
+
+    Optional<Division> findByUuid(UUID uuid);
+
+    @Query(value = "SELECT * from divisions d WHERE d.uuid in ?1", nativeQuery = true)
+    List<Division> findAllByUuidExists(List<UUID> ids);
+
+
+
 }
--- backend/SmartSecurity/src/main/java/com/isec/gps41/SmartSecurity/repository/UserRepository.java
+++ backend/SmartSecurity/src/main/java/com/isec/gps41/SmartSecurity/repository/UserRepository.java
@@ -11,12 +11,24 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByEmail(String email);
+
     User findByEmailAndActiveIsTrue(String email);
 
     User findByEmailAndActive(String email, boolean active);
 
+    Optional<User> findById(long id);
+
 
     boolean existsByEmail(String email);
 
+    List<User> findAllByRoleOrRoleAndActive(GrantedAuthority role1, GrantedAuthority role2, boolean active, Pageable pageable);
+
+    List<User> findAllByRoleAndActive(GrantedAuthority userRole, boolean active, Pageable pageable);
+    int countAllByRoleOrRoleAndActiveIsTrue(GrantedAuthority role, GrantedAuthority role2);
+
+    int countAllByRoleAndActiveIsTrue(GrantedAuthority role1);
+
+    Optional<User> findByUuid(UUID uuid);
 
 }
