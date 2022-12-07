e77ddab1dbf916dec677002d669e2ca602830c7b
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-12-07 17:16:05 +0000

    #23 - alarms  and monitorization component

---
+++ frontend/src/pages/administration/components/alarms/alarms.component.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from "react";
+
+export default function Alarms(props) {
+    return <div>Alarms</div>;
+}
---
+++ frontend/src/pages/administration/components/monitorization/monitorization.component.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from "react";
+
+export default function Monitorization(props) {
+    return <div>Monitorization</div>;
+}
